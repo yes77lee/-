307c11f187d23f4a2ee2f625eb4bae2e4cfe5c3c
yes77lee <yes77lee@naver.com>
2018-07-26 13:19:18 +0900

    day4

---
+++ function.py
@@ -0,0 +1,5 @@
+def hello():
+    name = input("what is your name")
+    print("hollo, " + name)
+
+hello()
---
+++ input.py
@@ -0,0 +1,18 @@
+# name = input("what is your name?")
+# print("Hello," + name)
+
+print("sum >")
+num1 = input("first number :")
+num2 = input("second number :")
+num1=int(num1)
+num2=int(num2)
+print(num1 + num2)
+print(num1 - num2)
+print(num1*num2)
+print(num1/num2)
+print(num1%num2)
+
+if(num1%2==0):
+    print("num1 is even")
+else:
+    print("num1 is odd")
